5867aa293f3719645f23f8def1b356ef33d05560
Tianle Huang <tianleh@amazon.com>
2021-08-25 22:17:28 +0000

    add cypress tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,20 @@
+import { openLandingPage } from 'test-library'
+
 describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
-    })
+    it('Gets, types and asserts', () => {
+      // console.log('re ' + openLandingPage)  
+      // cy.log('cy re is ' + openLandingPage)
+      cy.log('random re is ')
+      // openLandingPage()
+
+      // cy.contains('type').click()
+
+      // // Should be on a new URL which includes '/commands/actions'
+      // cy.url().should('include', '/commands/actions')
+
+      // // Get an input, type into it and verify that the value has been updated
+      // cy.get('.action-email')
+      //     .type('fake@email.com')
+      //     .should('have.value', 'fake@email.com')
   })
+})
